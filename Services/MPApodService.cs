fd17241fbd325b1485681410cf9bfcc436081c1c
Mundrack <mateopuga75@gmail.com>
2025-01-06 10:53:02 -0500

    CREAMOS LOS VIEWMODELS Y LOS APODS

---
+++ Services/MPApodService.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+using mpAPINasa.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mpAPINasa.Services
+{
+    public class MPApodService
+    {
+        public async Task<MPapod> GetImage(DateTime dt)
+        {
+            MPapod dto = null;
+            HttpResponseMessage response;
+            string requestUrl = $"https://api.nasa.gov/planetary/apod?date={dt.ToString("yyyy-MMdd")}&api_key=GXvdsvIUmbJdc0iAbaRv3xhImbqee3f9sVbRkeTw";
+            try
+            {
+                HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, requestUrl);
+                HttpClient client = new HttpClient();
+                response = await client.SendAsync(request);
+                if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    string json = await response.Content.ReadAsStringAsync();
+                    dto = JsonConvert.DeserializeObject<MPapod>(json);
+                }
+            }
+            catch (Exception ex)
+            {
+                string message = ex.Message;
+                throw;
+            }
+            return dto;
+        }
+    }
+}
